dc912c1e769db8e599c25103dba0561c0f0d4fa3
amanabdulla98 <amanabd98@gmail.com>
2022-09-09 19:58:44 +0530

    string

---
+++ string_basic.py
@@ -0,0 +1,44 @@
+my_string= 'hello'
+print(my_string)
+
+my_string = "hello"
+print(my_string)
+
+my_string = '''hello'''
+print(my_string)
+
+my_string = """hello,wellcome to
+                 the world of python
+                 
+                 hello once again"""
+print(my_string)
+
+print(my_string[0])
+print(my_string[2])
+
+
+name = 'vijay deenanath chauhan'
+
+print(name[6: -8])
+print(name[11: 15])
+
+print(name [::2])
+
+x= chr(65)
+print(x)
+x=chr(2365)
+print(x)
+x= chr(12365)
+print(x)
+
+for i in range(200,2000):
+    print(i, chr(i))
+
+w1='this '
+w2= ' is'
+w3 =' amazing'
+msg= w1 + w2 + w3
+print(msg)
+
+word =  ' hello'
+print(word * 10)
