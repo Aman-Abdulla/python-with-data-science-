a28a220477c5521595e0454644e8295a6a4bd925
Aman-Abdulla <amanabd98@gmail.com>
2022-11-10 19:58:28 +0530

    ML

--- Computer Vision/drawing.py
+++ Computer Vision/drawing.py
@@ -9,8 +9,13 @@ color_white = (255,255,255)  #white
 while cam.isOpened():
     status, image= cam.read()
     if status:
+        # draw a box 
+        cv2.rectangle(image,(5,5),(300,40),(0,255,0),-1)
         # add text
         cv2.putText(image,"Live Camera Feed",(10,30), font,1, color_black,2)
+        #add a clock
+        timestr = dt.now().strftime("%H:%M:%S")
+        cv2.putText(image,timestr,(10,70),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,0),2)
         cv2.imshow("live Camera", image)
         if cv2.waitKey(1) == 27: # 27 is the ASCII code of ESCAPE KEY
             break
